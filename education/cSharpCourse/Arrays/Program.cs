85d1d270a10afe6cb2b4abcbd87093b410717d91
Atakan Demir <demiratakan2002@gmail.com>
2023-08-16 04:32:39 +0300

    Atakan Demir

---
+++ education/cSharpCourse/Arrays/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Sa1ihl";
+            
+
+            string[] students2 = {"Engin","Derin","Salih"};
+
+            foreach (var student in students2)
+            {
+
+                //Console.WriteLine(student);
+                
+            }
+
+            // Cok boyutlu diziler
+
+            string[,] regions = new string[5, 3]//5 satir 3 sutun
+            {
+                {"İstanbul","Edirne","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale" },
+                {"Antalya","Adana","Mersin" },
+                {"Rize","Trabzon","Samsun" },
+                {"İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("*******");
+            }
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ education/cSharpCourse/Loops/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is prime number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a prime number");
+            }
+
+            /*
+            // for
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // while
+            int number = 100;
+            while (number >= 0)
+            {
+                number--;
+                Console.WriteLine(number);
+            }
+
+            // do while
+            int number2 = 10;
+            do
+            {
+                Console.WriteLine(number2);
+                number2--;
+            } while (number2>=11);
+
+            // foreach
+            string[] students = { "Engin", "Atakan", "Emral" };
+            foreach (var student in students)
+            {
+                //student = "başka bir string"  >>> foreach dongu icinde degisken degistirmeye izin vermez
+                Console.WriteLine(student);
+            }
+            */
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int num)
+        {
+            bool result = true;
+
+            for (int i = 2; i < num - 1; i++)
+            {
+                if (num % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+
+            }
+            return result;
+
+        }
+    }
+}
