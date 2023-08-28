cc5e920b900610b9ea5e26b0388a556440acbb27
Atakan Demir <demiratakan2002@gmail.com>
2023-08-29 01:44:17 +0300

    Atakan Demir

---
+++ education/cSharpCourse/Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Product
+    {
+
+    }
+    interface IRepository<T>
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
