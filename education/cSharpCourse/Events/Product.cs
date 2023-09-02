43a986db378ab89ea1ae000a589c0d4780883611
Atakan Demir <demiratakan2002@gmail.com>
2023-09-02 03:54:23 +0300

    Atakan Demir

---
+++ education/cSharpCourse/Events/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    // elci
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        //event tanimlama
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock
+        {
+            get { return _stock; }
+            set
+            {
+                _stock = value;
+                //sart saglanirsa StockControlEvent i calistir
+                if (value<=15 &&  StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            }
+        }
+
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{1} Stock amount: {0}", Stock,ProductName);
+        }
+    }
+}
---
+++ education/cSharpCourse/Events/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //stok da 50 adet var
+            Product harddisk = new Product(50)
+            {
+                ProductName = "Hard Disk"
+            };
+
+            Product phone = new Product(50)
+            {
+                ProductName = "Phone"
+            };
+            // phone StockControlEvent olayina abone oldu
+            phone.StockControlEvent += Phone_StockControlEvent;
+            for (int i = 0; i < 10; i++)
+            {
+                //satislar yapiliyor
+                harddisk.Sell(10);
+                phone.Sell(10);
+
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Phone_StockControlEvent()
+        {
+            Console.WriteLine("Phone about to finish !!!");
+        }
+    }
+}
